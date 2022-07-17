b2196592e6c4fc5de372f3f36c624b9eca1c6d5a
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-17 17:05:27 -0500

    Add Links to Nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -29,10 +30,18 @@ function Navbar() {
 
         <div id='navbarBasicExample' className='navbar-menu'>
           <div className='navbar-end'>
-            <a className='navbar-item'>Home</a>
-            <a className='navbar-item'>About</a>
-            <a className='navbar-item'>Work</a>
-            <a className='navbar-item'>Contact</a>
+            <Link to='/' className='navbar-item'>
+              Home
+            </Link>
+            <Link to='about' className='navbar-item'>
+              About
+            </Link>
+            <Link to='projects' className='navbar-item'>
+              Projects
+            </Link>
+            <Link to='contact' className='navbar-item'>
+              Contact
+            </Link>
           </div>
         </div>
       </nav>
--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Navbar />
     <BrowserRouter>
+      <Navbar />
       <App />
+      <Footer />
     </BrowserRouter>
-    <Footer />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
