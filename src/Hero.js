f27d8f44c4e424660e94ae834f145fefb3bdc696
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-17 13:31:00 -0500

    Style cleanup

--- src/Hero.js
+++ src/Hero.js
@@ -2,17 +2,37 @@ import React from 'react';
 
 function Hero() {
   return (
-    <section class='hero is-medium'>
+    <section style={{maxWidth: 800}}class='hero is-small container'>
       <div class='hero-body'>
         <p
-          style={{ fontSize: '24vh', fontFamily: 'Philosopher' }}
-          class='title'>
-          Hi!
+          style={{ fontFamily: 'Philosopher', marginBottom: '50px' }}
+          className='title has-text-centered'>
+          Hi! I'm Jacquelyn!
         </p>
-        <p
-          style={{ fontSize: '24vh', fontFamily: 'Philosopher' }}
-          class='title'>
-          Glad ya made it!
+        <p style={{ fontFamily: 'Philosopher' }} class='subtitle'>
+          I’m based in Dallas Texas. I enjoy working with holistic, health and
+          wellness, sports, music, and art companies to create user centric
+          products. My background isn’t typical, its very deep and meaningful,
+          in ways that are unknown to most until they have to meet their
+          Mortician, in one of the hardest moments life can deal you.
+        </p>
+        <p style={{ fontFamily: 'Philosopher' }} class='subtitle px-1'>
+          My background isn’t typical, its very deep and meaningful, in ways
+          that are unknown to most until they have to meet their Mortician, in
+          one of the hardest moments life can deal you.
+        </p>
+        <p style={{ fontFamily: 'Philosopher' }} class='subtitle'>
+          As a Mortician my focus was not only on the deceased, but also on the
+          living and how I could provide them with the most memorable and
+          positive send off for someone whom they cared deeply for.
+        </p>
+        <p style={{ fontFamily: 'Philosopher' }} class='subtitle'>
+          I can positively say this is user centric in its most raw form. My
+          passion for the people is a strength and honor built within the very
+          bones of my body. I am hungry for knowledge. I am a driven and curious
+          being ready to take on anything and learn. This is my superpower and
+          also the core strength that drives my passion for Ux design. If you’d
+          like to know more, I’d love to connect.
         </p>
       </div>
     </section>
--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Content from './Content';
+import Project from './Project';
+
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
+      <Project />
     </>
   );
 }
