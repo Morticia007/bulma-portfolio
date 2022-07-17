7a13e818fa8c17137765ed08438b7919b822adfd
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-17 14:04:29 -0500

    Add projects component

--- src/Hero.js
+++ src/Hero.js
@@ -2,31 +2,31 @@ import React from 'react';
 
 function Hero() {
   return (
-    <section style={{maxWidth: 800}}class='hero is-small container'>
-      <div class='hero-body'>
+    <section style={{maxWidth: 800}} class='hero is-small container'>
+      <div className='hero-body'>
         <p
           style={{ fontFamily: 'Philosopher', marginBottom: '50px' }}
           className='title has-text-centered'>
           Hi! I'm Jacquelyn!
         </p>
-        <p style={{ fontFamily: 'Philosopher' }} class='subtitle'>
+        <p style={{ fontFamily: 'Philosopher' }} className='subtitle'>
           I’m based in Dallas Texas. I enjoy working with holistic, health and
           wellness, sports, music, and art companies to create user centric
           products. My background isn’t typical, its very deep and meaningful,
           in ways that are unknown to most until they have to meet their
           Mortician, in one of the hardest moments life can deal you.
         </p>
-        <p style={{ fontFamily: 'Philosopher' }} class='subtitle px-1'>
+        <p style={{ fontFamily: 'Philosopher' }} className='subtitle px-1'>
           My background isn’t typical, its very deep and meaningful, in ways
           that are unknown to most until they have to meet their Mortician, in
           one of the hardest moments life can deal you.
         </p>
-        <p style={{ fontFamily: 'Philosopher' }} class='subtitle'>
+        <p style={{ fontFamily: 'Philosopher' }} className='subtitle'>
           As a Mortician my focus was not only on the deceased, but also on the
           living and how I could provide them with the most memorable and
           positive send off for someone whom they cared deeply for.
         </p>
-        <p style={{ fontFamily: 'Philosopher' }} class='subtitle'>
+        <p style={{ fontFamily: 'Philosopher' }} className='subtitle'>
           I can positively say this is user centric in its most raw form. My
           passion for the people is a strength and honor built within the very
           bones of my body. I am hungry for knowledge. I am a driven and curious
