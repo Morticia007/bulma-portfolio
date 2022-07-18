c9ef847bf905dc139d16b16796e181534c14cea3
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-18 10:46:24 -0500

    Add Education component

--- src/About.js
+++ src/About.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Education from './Education';
+
 function About() {
   return (
     <div className='content is-large'>
@@ -37,21 +39,7 @@ function About() {
         </p>
       </div>
       <hr />
-      <div style={{ padding: 40 }}>
-        <h1 style={{ marginBottom: 40, marginTop: 40 }}>Education</h1>
-        <div className='block'>
-          <h3 className='subtitle'>GENERAL ASSSEMBY UX BOOTCAMP</h3>
-          <p className='subtitle'>April 2019 - July 2019P</p>
-        </div>
-        <div className='block'>
-          <h3 className='subtitle'>NEWCAMP FULLSTACK DEVELOPER BOOTCAMP</h3>
-          <p className='subtitle'>November 2020 - May 2021</p>
-        </div>
-        <div className='block'>
-          <h3 className='subtitle'>DESIGN LAB UX/UI ACADEMY</h3>
-          <p className='subtitle'>May 2022 - Present</p>
-        </div>
-      </div>
+      <Education />
     </div>
   );
 }
---
+++ src/Education.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Education() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1 style={{ marginBottom: 40, marginTop: 40 }}>Education</h1>
+      <div className='block'>
+        <h3 className='subtitle'>GENERAL ASSSEMBY UX BOOTCAMP</h3>
+        <p className='subtitle'>April 2019 - July 2019P</p>
+      </div>
+      <div className='block'>
+        <h3 className='subtitle'>NEWCAMP FULLSTACK DEVELOPER BOOTCAMP</h3>
+        <p className='subtitle'>November 2020 - May 2021</p>
+      </div>
+      <div className='block'>
+        <h3 className='subtitle'>DESIGN LAB UX/UI ACADEMY</h3>
+        <p className='subtitle'>May 2022 - Present</p>
+      </div>
+    </div>
+  );
+}
+export default Education;
