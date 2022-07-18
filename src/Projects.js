ef1bc60da903413819608d03f2cc9c40bf54bdcf
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-18 09:15:00 -0500

    Fix image layout

--- src/Projects.js
+++ src/Projects.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Project from './Project';
 import Frame from './assets/Frame 10209.png';
 
+const projectList = [
+  { image: Frame },
+  { image: Frame },
+  { image: Frame },
+];
+
 function Projects() {
   return (
     <section style={{ marginTop: 80 }} className='container'>
       <h1 className='title has-text-centered'>Projects</h1>
       <div className='columns'>
-        <Project projectImage={Frame} />
-        <Project projectImage={Frame} />
-        <Project projectImage={Frame} />
+        {projectList.map((item) => (
+          <Project projectImage={item.image} />
+        ))}
       </div>
     </section>
   );
