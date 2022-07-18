f92881fe8f864c2dc8ca864117b18a1918a2bdc6
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-17 21:37:06 -0500

    Add contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact() {
+  const [submit, setSubmit] = useState(false);
+  const [deleteNotification, setDeleteNotification] = useState(false);
+
+  const handleClick = () => {
+    setSubmit(true);
+  };
+
+  const handleDelete = () => {
+    setDeleteNotification(true);
+  };
+
+  if (submit) {
+    if (deleteNotification) {
+      return (
+        <div style={{ marginTop: 80 }} className='container content is-medium'>
+          <h1>I'd love to connect!</h1>
+          <h1>Send me a message</h1>
+          <form>
+            <div className='columns'>
+              <div class='control column'>
+                <input
+                  class='input is-large'
+                  type='email'
+                  placeholder='Enter Your Name '
+                />
+              </div>
+              <div class='control column'>
+                <input
+                  class='input is-large'
+                  type='email'
+                  placeholder='Enter Your Email'
+                />
+              </div>
+            </div>
+            <div class='control block'>
+              <textarea
+                rows='10'
+                class='textarea is-large'
+                placeholder='Enter Your Message'></textarea>
+            </div>
+            <button onClick={handleClick} class='button is-large'>
+              Send
+            </button>
+          </form>
+        </div>
+      );
+    }
+    return (
+      <div class='notification is-success is-light'>
+        <button onClick={handleDelete} class='delete'></button>
+        Your email has been sent!
+      </div>
+    );
+  }
   return (
     <div style={{ marginTop: 80 }} className='container content is-medium'>
       <h1>I'd love to connect!</h1>
@@ -22,12 +76,15 @@ function Contact() {
             />
           </div>
         </div>
-        <div class='control'>
+        <div class='control block'>
           <textarea
             rows='10'
             class='textarea is-large'
             placeholder='Enter Your Message'></textarea>
         </div>
+        <button onClick={handleClick} class='button is-large'>
+          Send
+        </button>
       </form>
     </div>
   );
