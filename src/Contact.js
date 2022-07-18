07e982fea0ad1306cf8a388c66e16791e0611908
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-17 21:15:23 -0500

    Add contact page

--- src/Contact.js
+++ src/Contact.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div className='container content is-medium'>
-      <h1>Contact</h1>
+    <div style={{ marginTop: 80 }} className='container content is-medium'>
+      <h1>I'd love to connect!</h1>
+      <h1>Send me a message</h1>
+      <form>
+        <div className='columns'>
+          <div class='control column'>
+            <input
+              class='input is-large'
+              type='email'
+              placeholder='Enter Your Name '
+            />
+          </div>
+          <div class='control column'>
+            <input
+              class='input is-large'
+              type='email'
+              placeholder='Enter Your Email'
+            />
+          </div>
+        </div>
+        <div class='control'>
+          <textarea
+            rows='10'
+            class='textarea is-large'
+            placeholder='Enter Your Message'></textarea>
+        </div>
+      </form>
     </div>
   );
 }
