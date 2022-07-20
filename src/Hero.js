dea1cc28fad2116903cf067537217b62c3e804ec
Jacquelyn Elder <jacquelyn07@mac.com>
2022-07-20 16:33:12 -0500

    Hero fixes

--- src/Hero.js
+++ src/Hero.js
@@ -5,17 +5,11 @@ function Hero() {
     <>
       <section style={{ background: '#609497' }} className='hero is-medium'>
         <div className='hero-body'>
-          <p
-            style={{ color: 'white' }}
-            className='title has-text-centered block'>
-            Hi! Glad you made it!
-          </p>
-        </div>
-      </section>
-      <section className='hero is-medium'>
-        <div className='hero-body'>
           <div className='columns'>
-            <p className='title column is-half is-offset-one-quarter has-text-left bloc is-large'>
+            <p
+              style={{ color: 'white' }}
+              className='title column is-half is-offset-one-quarter has-text-left bloc is-large is-size-1'
+            >
               I’m based in Dallas Texas. I enjoy working with holistic, health
               and wellness, sports, music, and art companies to create user
               centric products.
