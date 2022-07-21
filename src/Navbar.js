b0c594fa76c858151edffce072dd21b623989701
Jacquelyn René Hagman <jacquelyn07@mac.com>
2022-07-21 13:08:03 -0500

    Add logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './assets/logo-5.png';
+import logo from './assets/logo-6.png';
 
 function Navbar() {
   return (
