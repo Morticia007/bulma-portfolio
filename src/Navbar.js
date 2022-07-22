2d9b02b31e9168c820f32a2c746b19a206d69dcc
Jacquelyn Elder <jacquelyn07@mac.com>
2022-07-21 19:34:04 -0500

    Add Mobile Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,44 +4,61 @@ import logo from './assets/logo-14.png';
 
 function Navbar() {
   return (
-    <div className='container-fluid'>
-      <nav
-        style={{ height: 96 }}
-        className='navbar is-spaced'
-        role='navigation'
-        aria-label='main navigation'
-      >
-        <div className='navbar-brand'>
-          <a className='navbar-item' href='https://jacquelynhagman.design'>
-            <img
-              alt='logo`'
-              style={{ maxHeight: 86 }}
-              src={logo}
-            />
-          </a>
+    <>
+      <div className='container-fluid is-hidden-desktop'>
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Link to='/' className='navbar-item'>
+            <img alt='logo' src={logo} />
+          </Link>
+          <Link style={{ marginLeft: 'auto' }} to='/' className='navbar-item'>
+            Home
+          </Link>
+          <Link to='about' className='navbar-item'>
+            About
+          </Link>
+          <Link to='work' className='navbar-item'>
+            Work
+          </Link>
+          <Link to='contact' className='navbar-item'>
+            Contact
+          </Link>
         </div>
-        <div
-          style={{ fontWeight: 500 }}
-          id='navbarBasicExample'
-          className='navbar-menu'
+      </div>
+      <div className='container-fluid is-hidden-mobile'>
+        <nav
+          style={{ height: 96 }}
+          className='navbar is-spaced'
+          role='navigation'
+          aria-label='main navigation'
         >
-          <div className='navbar-end'>
-            <Link to='/' className='navbar-item'>
-              Home
-            </Link>
-            <Link to='about' className='navbar-item'>
-              About
-            </Link>
-            <Link to='work' className='navbar-item'>
-              Work
-            </Link>
-            <Link to='contact' className='navbar-item'>
-              Contact
-            </Link>
+          <div className='navbar-brand'>
+            <a className='navbar-item' href='https://jacquelynhagman.design'>
+              <img alt='logo`' style={{ maxHeight: 86 }} src={logo} />
+            </a>
           </div>
-        </div>
-      </nav>
-    </div>
+          <div
+            style={{ fontWeight: 500 }}
+            id='navbarBasicExample'
+            className='navbar-menu'
+          >
+            <div className='navbar-end'>
+              <Link to='/' className='navbar-item'>
+                Home
+              </Link>
+              <Link to='about' className='navbar-item'>
+                About
+              </Link>
+              <Link to='work' className='navbar-item'>
+                Work
+              </Link>
+              <Link to='contact' className='navbar-item'>
+                Contact
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
   );
 }
 
